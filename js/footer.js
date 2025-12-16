b5b25674ae92016e196255f26e76af3683b7464b
Constant NGUYEN <constant.nguyen@iut-rodez.fr>
2025-12-16 01:20:48 +0100

    Ajout A propos et footer

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+const languePasPossible = document.getElementsByClassName("langueChoix");
+for (let i = 1; i < languePasPossible.length; i++) {
+
+  languePasPossible[i].addEventListener("click", function () {
+    alert("Fonctionnalité de changement de langue pas implémentée, français uniquement");
+  });
+
+}
